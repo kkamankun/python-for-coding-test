3b18297b20a0bbda2108b42fe9904a8226ebe53a
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2021-03-17 17:18:19 +0900

    Create q4_만들 수 없는 금액.py

---
+++ 1회/그리디 문제/q4_만들 수 없는 금액.py
@@ -0,0 +1,14 @@
+# 나의 코드
+n = int(input())
+input_data = list(map(int, input().split()))
+input_data.sort()
+cnt_list = [0] * 100
+for i in range(0, n - 1):
+  for j in range(i + 1, n):
+    cnt_list[input_data[i] + input_data[j]] += 1
+for i in range(0, n):
+  if cnt_list[i] == 0:
+    print(i + 1)
+    break
+
+# 책 코드
