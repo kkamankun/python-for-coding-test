86889d7239a1600d3aa4eaf679170c259e477e57
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2021-03-22 14:25:48 +0900

    Update q4_만들 수 없는 금액.py

--- 1회/그리디 문제/q4_만들 수 없는 금액.py
+++ 1회/그리디 문제/q4_만들 수 없는 금액.py
@@ -15,6 +15,7 @@ for i in range(0, n):
 '''
 현재 상태를 '1부터 target-1까지의 모든 금액을 만들 수 있는 상태'라고 보자.
 이때 매번 target인 금액도 만들 수 있는지 체크하는 것이다.
+만약 해당 금액을 만들 수 있다면, target의 값을 업데이트하면 된다.
 '''
 n = int(input())
 input_data = list(map(int, input().split()))
