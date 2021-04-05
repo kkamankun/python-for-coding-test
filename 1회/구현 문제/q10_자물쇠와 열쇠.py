14d7abaef5475d638a18178c2c7144bbfac061d0
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2021-04-05 17:33:12 +0900

    Update q10_자물쇠와 열쇠.py

--- 1회/구현 문제/q10_자물쇠와 열쇠.py
+++ 1회/구현 문제/q10_자물쇠와 열쇠.py
@@ -1,3 +1,46 @@
+# 나의 풀이
+import copy
+
+
+def check(n, arr):
+    for r in range(n):
+        for c in range(n):
+            if arr[r + n][c + n] != 1:
+                return False
+    return True
+
+
+def rotate_a_matrix_by_90_degree(key):
+    m = len(key)
+    result = [[0] * m for _ in range(m)]
+    for r in range(m):
+        for c in range(m):
+            result[c][m - 1 - r] = key[r][c]
+    return result
+
+
+def solution(key, lock):
+    m, n = len(key), len(lock)
+    array = [[0] * 3 * n for _ in range(3 * n)]
+    for r in range(n):
+        for c in range(n):
+            array[r + n][c + n] = lock[r][c]
+    ori = copy.deepcopy(array)
+
+    for i in range(4):
+        for x in range(2 * n):
+            for y in range(2 * n):
+                for r in range(m):
+                    for c in range(m):
+                        array[r + x][c + y] += key[r][c]
+                if check(n, array):
+                    return True
+                else:
+                    array = copy.deepcopy(ori)
+        key = rotate_a_matrix_by_90_degree(key)
+    return False
+
+# 책 풀이
 def rotate_a_matrix_by_90_degree(a):
   n = len(a)
   m = len(a)
