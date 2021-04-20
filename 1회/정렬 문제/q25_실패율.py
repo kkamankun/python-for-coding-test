13036e0820c2476b1ac324ec64dbd016be5328ef
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2021-04-20 14:15:27 +0900

    Update q25_실패율.py

--- 1회/정렬 문제/q25_실패율.py
+++ 1회/정렬 문제/q25_실패율.py
@@ -1,3 +1,4 @@
+# 나의 풀이
 def solution(N, stages):
     answer = []
     t = len(stages)
@@ -13,3 +14,30 @@ def solution(N, stages):
 
     answer.sort(key=lambda x: x[1], reverse=True)
     return [i[0] for i in answer]
+
+# 책의 풀이
+def solution(N, stages):
+    answer = []
+    length = len(stages)
+
+    # 스테이지 번호를 1부터 N까지 증가시키며
+    for i in range(1, N + 1):
+        # 해당 스테이지에 머물러 있는 사람의 수 계산
+        count = stages.count(i)
+
+        # 실패율 계산
+        if length == 0:
+            fail = 0
+        else:
+            fail = count / length
+
+        # 리스트에 (스테이지 번호, 실패율) 원소 삽입
+        answer.append((i, fail))
+        length -= count
+
+    # 실패율을 기준으로 각 스테이지를 내림차순 정렬
+    answer = sorted(answer, key=lambda t: t[1], reverse=True)
+
+    # 정렬된 스테이지 번호 출력
+    answer = [i[0] for i in answer]
+    return answer
