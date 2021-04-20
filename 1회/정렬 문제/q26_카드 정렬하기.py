246695d3c5253d26553a3f6c8a989e401ff9d613
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2021-04-20 14:37:23 +0900

    Update q26_카드 정렬하기.py

--- 1회/정렬 문제/q26_카드 정렬하기.py
+++ 1회/정렬 문제/q26_카드 정렬하기.py
@@ -1,3 +1,4 @@
+# 나의 풀이
 from queue import PriorityQueue
 que = PriorityQueue()
 n = int(input())
@@ -11,3 +12,28 @@ for i in range(n-1):
     que.put(s)
 
 print(sum(cnt))
+
+# 책의 풀이
+import heapq
+
+n = int(input())
+
+# 힙(Heap)에 초기 카드 묶음을 모두 삽입
+heap = []
+for i in range(n):
+    data = int(input())
+    heapq.heappush(heap, data)
+
+result = 0
+
+# 힙(Heap)에 원소가 1개 남을 때까지
+while len(heap) != 1:
+    # 가장 작은 2개의 카드 묶음 꺼내기
+    one = heapq.heappop(heap)
+    two = heapq.heappop(heap)
+    # 카드 묶음을 합쳐서 다시 삽입
+    sum_value = one + two
+    result += sum_value
+    heapq.heappush(heap, sum_value)
+
+print(result)
